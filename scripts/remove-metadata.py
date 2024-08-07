87d301e1fb2eace5e81dde775b4d4a398405358d
Evgeny Arbatov <arbatovevgeny@gmail.com>
2024-08-07 13:20:15 +0700

    Remove extra points

--- scripts/remove-metadata.py
+++ scripts/remove-metadata.py
@@ -2,6 +2,19 @@ import sys
 import gpxpy
 
 def remove_metadata(gpx):
+    (
+        gpx.name, 
+        gpx.creator, 
+        gpx.description, 
+        gpx.time,
+        gpx.bounds,
+    ) = (
+        None,
+        None,
+        None,
+        None,
+        None,
+    )
     for track in gpx.tracks:
         track.name, track.type = None, None
         for segment in track.segments:
